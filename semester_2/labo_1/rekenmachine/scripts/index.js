121ad86d04a19274998d45baacee52cabe6b14f0
str-efrain <efrain.mouton@student.vives.be>
2025-02-11 11:50:19 +0100

    Labo 1

--- semester_2/labo_1/rekenmachine/scripts/index.js
+++ semester_2/labo_1/rekenmachine/scripts/index.js
@@ -25,15 +25,42 @@ const optellen = () => {
 }
 
 const aftrekken = () => {
-	// analoog
+	let txtOutput=document.getElementById("txtOutput");
+	let txtLinks=document.getElementById("txtLinks");
+	let txtRechts=document.getElementById("txtRechts");
+
+	let g1=parseInt(txtLinks.value, 10);
+	let g2=parseInt(txtRechts.value, 10);
+	let resultaat = g1-g2;
+
+	let resultaatTekst=g1+" -  "+g2+" = "+resultaat;
+	txtOutput.innerHTML=resultaatTekst;
 }
 
 const vermenigvuldigen = () => {
-	// analoog
+	let txtOutput=document.getElementById("txtOutput");
+	let txtLinks=document.getElementById("txtLinks");
+	let txtRechts=document.getElementById("txtRechts");
+
+	let g1=parseInt(txtLinks.value, 10);
+	let g2=parseInt(txtRechts.value, 10);
+	let resultaat = g1*g2;
+
+	let resultaatTekst=g1+" * "+g2+" = "+resultaat;
+	txtOutput.innerHTML=resultaatTekst;
 }
 
 const delen = () => {
-	// analoog
+	let txtOutput=document.getElementById("txtOutput");
+	let txtLinks=document.getElementById("txtLinks");
+	let txtRechts=document.getElementById("txtRechts");
+
+	let g1=parseInt(txtLinks.value, 10);
+	let g2=parseInt(txtRechts.value, 10);
+	let resultaat = g1/g2;
+
+	let resultaatTekst=g1+" / "+g2+" = "+resultaat;
+	txtOutput.innerHTML=resultaatTekst;
 }
  
 window.addEventListener('load',setup); 
